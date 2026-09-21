f898ec45e12bfa819a56f67c7aae376db2b47952
thirteen06 <rune@thirteen06.net>
2026-09-21 15:25:51 +0200

    MorePrivacy. er blevet gjort private og der er lavet en contructor.

--- src/MorePrivacy.java
+++ src/MorePrivacy.java
@@ -3,16 +3,21 @@
 */
 
 public class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
 
-void main() {
-    Person p = new Person();
 
-    p.name = "Alice";
-    p.age = 20;
+void main() {
+    Person p = new Person("Alice", 20);
+
 
     IO.println(p.name);
     IO.println(p.age);
+
 }
